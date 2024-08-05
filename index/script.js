9594320c4f0f1f1c095caee3370b907e6c14b3c4
Tushar <ts6308969@gmail.com>
2024-08-05 23:45:12 +0530

    new changes

--- index/script.js
+++ index/script.js
@@ -1,16 +1,5 @@
 function handleSubmit(event){
-    event.preventDefault();
-    let fullname=document.getElementById('fullname').value.trim();
-    if(fullname===""){
-        alert("please enter your name")
-    }
-    else if(fullname.length<3) {
-        alert("please enter yourfull name")
-    }
-    let nameError =document.getElementById('nameError')
-nameError.innerHTML="";
-if(nameError===""){
-    nameError.innerHTML="please enter your name"
+    let a=document.getElementById('fullname').
+    console.log(a)
 }
-console.log(fullname);
-}
+
