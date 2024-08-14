4674bf85471a1015edf1544918994bd605083492
Tushar <ts6308969@gmail.com>
2024-08-14 19:49:24 +0530

    adding login and registration page

--- route.js
+++ route.js
@@ -4,8 +4,86 @@ const mysql_connect=require("./mysql_connector")
 const connection = require('./mysql_connector')
 router.get("/",(req,res)=>
     {
-        res.render("index")
+        res.render("login")
         res.end()
     }
     )
+    router.use("/login",(req,res)=>
+        {
+            if(req.method==='GET'){
+                res.render("login")
+                res.end() 
+            }
+            else{
+                mysql_connect.getConnection((err,connection)=>
+                {
+                    if(err){
+                        connection.release()
+                        res.send(err)
+                        res.end()
+                    }
+                    else{
+                        var email=req.body.email
+                    var password=req.body.password
+        const q=`select * from register where email='${email}' and password='${password}'`
+        connection.query(q,(err,data)=>
+        {
+            if(err){
+                connection.release()
+                        res.render(err)
+                        res.end()
+            }
+            else{
+                     if(data.length>0)
+                     {
+                        res.render("index")
+                        res.end()
+                     }
+                     else 
+                     {
+                      res.render('login',{message:"  incorrect password"})
+                     }
+            } 
+        })
+                        }
+                })
+            }  
+        }
+        )
+        router.use("/register",(req,res)=>
+            {
+                if(req.method==='GET'){
+                    res.render("register")
+                    res.end()
+                }
+                else{
+                    mysql_connect.getConnection((err,connection)=>
+                    {
+                        if(err){
+                            connection.release()
+                            res.send(err)
+                            res.end()
+                        }
+                        else{
+                            var name=req.body.name
+                            var email=req.body.email
+                        var password=req.body.password
+                        var confirm_password=req.body.confirm_password
+            const q=`insert into register (name,email,password,confirm_password)values('${name}','${email}','${password}','${confirm_password}')`
+            connection.query(q,(err)=>
+            {
+                if(err){
+                    connection.release()
+                            res.render(err)
+                            res.end()
+                }
+                else{
+                    res.render('login',{message:email+" Added Successfully"})
+                }
+            })
+                            }
+                    })
+                }  
+            }
+            )
     module.exports = router;
