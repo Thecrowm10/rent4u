6ee366750190fd2717bb037163b6e4a96657d360
Tushar <ts6308969@gmail.com>
2024-12-17 12:30:14 +0530

    email js added

--- route.js
+++ route.js
@@ -1,121 +1,72 @@
-const express=require('express')
-const router=express.Router()
-const mysql_connect=require("./mysql_connector")
-const connection = require('./mysql_connector')
-router.get("/",(req,res)=>
-    {
-        res.render("index")
-        res.end()
-    }
-    )
-    router.use("/login",(req,res)=>
-        {
-            if(req.method==='GET'){
-                res.render("login")
-                res.end() 
+const express = require('express');
+const router = express.Router();
+const mysql_connect = require('./mysql_connector');
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+router.route('/login')
+    .get((req, res) => {
+        res.render('login');
+    })
+    .post((req, res) => {
+        mysql_connect.getConnection((err, connection) => {
+            if (err) {
+                return res.status(500).send('Database Connection Failed');
             }
-            else{
-                mysql_connect.getConnection((err,connection)=>
-                {
-                    if(err){
-                        connection.release()
-                        res.send(err)
-                        res.end()
-                    }
-                    else{
-                        var email=req.body.email
-                    var password=req.body.password
-        const q=`select * from register where email='${email}' and password='${password}'`
-        connection.query(q,(err,data)=>
-        {
-            if(err){
-                connection.release()
-                        res.render(err)
-                        res.end()
+
+            const { email, password } = req.body;
+            const query = 'SELECT * FROM register WHERE email = ? AND password = ?';
+
+            connection.query(query, [email, password], (err, results) => {
+                connection.release();
+
+                if (err) {
+                    return res.status(500).send('Database Query Failed');
+                }
+
+                if (results.length > 0) {
+                    res.render('index');
+                } else {
+                    res.render('login', { message: 'Incorrect email or password' });
+                }
+            });
+        });
+    });
+
+router.route('/register')
+    .get((req, res) => {
+        res.render('register');
+    })
+    .post((req, res) => {
+        mysql_connect.getConnection((err, connection) => {
+            if (err) {
+                return res.status(500).send('Database Connection Failed');
             }
-            else{
-                     if(data.length>0)
-                     {
-                        res.render("index")
-                        res.end()
-                     }
-                     else 
-                     {
-                      res.render('login',{message:"  incorrect password"})
-                     }
-            } 
-        })
-                        }
-                })
-            }  
-        }
-        )
-        router.use("/register",(req,res)=>
-            {
-                if(req.method==='GET'){
-                    res.render("register")
-                    res.end()
+
+            const { name, email, password, image } = req.body;
+            const query = 'INSERT INTO register (name, email, password, image) VALUES (?, ?, ?, ?)';
+
+            connection.query(query, [name, email, password, image], (err) => {
+                connection.release();
+
+                if (err) {
+                    return res.status(500).send('Database Query Failed');
                 }
-                else{
-                    mysql_connect.getConnection((err,connection)=>
-                    {
-                        if(err){
-                            connection.release()
-                            res.send(err)
-                            res.end()
-                        }
-                        else{
-                            var name=req.body.name
-                            var email=req.body.email
-                        var password=req.body.password
-                        var image=req.body.image
-            const q=`insert into register (name,email,password,confirm_password,image)values('${name}','${email}','${password}','${image}')`
-            
-            connection.query(q,(err)=>
-            {
-                if(err){
-                    connection.release()
-                            res.render(err)
-                            res.end()
-                }
-                else{
-                    res.render('login',{message:name+" Added Successfully"})
-                }
-            })
-                            }
-                    })
-                }  
-            }
-            )
-            
- router.use("/index",(req,res)=>
-                {
-                    res.render("index")
-                    res.end()
-                })
-router.use("/about",(req,res)=>
-            {
-                res.render("about")
-                res.end()
-            })
-router.use("/services",(req,res)=>
-                {
-                    res.render("services")
-                    res.end()
-                })
-router.use("/apartment",(req,res)=>
-                    {
-                        res.render("apartment")
-                        res.end()
-                    })
-router.use("/blog",(req,res)=>
-                        {
-                            res.render("blog")
-                            res.end()
-                        })
-router.use("/contact",(req,res)=>
-                         {
-                                res.render("contact")
-                                res.end()
-                            })
-    module.exports = router;
+
+                res.render('login', { message: `${name} Added Successfully` });
+            });
+        });
+    });
+
+router.get('/index', (req, res) => res.render('index'));
+router.get('/about', (req, res) => res.render('about'));
+router.get('/services', (req, res) => res.render('services'));
+router.get('/apartment', (req, res) => res.render('apartment'));
+router.get('/blog', (req, res) => res.render('blog'));
+router.get('/contact', (req, res) => res.render('contact'));
+
+module.exports = router;
