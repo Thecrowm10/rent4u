134f736c947be13d54358dc6dc07950c3badf729
Tushar Sharma <ts6308969@gmail.com>
2025-11-06 11:15:49 +0530

    Added environment variables for security

--- mysql_connector.js
+++ mysql_connector.js
@@ -1,14 +1,14 @@
 var mysql = require("mysql");
 
+// Code ab password yahaan se padhega
 var connection = mysql.createPool({
-    host: "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
-    port: 4000, // TiDB uses port 4000
-    user: "u3Hu4P8W7VT8pk1.root",
-    password: "pvnvl2VAxa30rM37",
-    database: "test", // TiDB's default database is 'test'
-    multipleStatements: true, // This is from your original file
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    multipleStatements: true,
     ssl: {
-        // This is required for TiDB Cloud to connect
         rejectUnauthorized: false 
     }
 });
