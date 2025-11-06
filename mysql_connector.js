aaf9439a2be39710147c6bf711804a2a0a9516dc
Tushar Sharma <ts6308969@gmail.com>
2025-11-06 10:47:30 +0530

    Update mysql_connector.js

--- mysql_connector.js
+++ mysql_connector.js
@@ -1,10 +1,16 @@
-var mysql=require("mysql");
+var mysql = require("mysql");
 
 var connection = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"rent4u",
-    multipleStatements: true
+    host: "gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+    port: 4000, // TiDB uses port 4000
+    user: "u3Hu4P8W7VT8pk1.root",
+    password: "pvnvl2VAxa30rM37",
+    database: "test", // TiDB's default database is 'test'
+    multipleStatements: true, // This is from your original file
+    ssl: {
+        // This is required for TiDB Cloud to connect
+        rejectUnauthorized: false 
+    }
 });
-module.exports=connection;
+
+module.exports = connection;
